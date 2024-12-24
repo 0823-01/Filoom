0ca9a71e377435a64846a2ad0212559a8c3b5132
HYECOKE <hhw100808@gmail.com>
2024-12-24 17:44:56 +0900

    241224혜원 커밋
    커밋

--- Filoom/src/main/java/com/kh/filoom/event/model/service/EventService.java
+++ Filoom/src/main/java/com/kh/filoom/event/model/service/EventService.java
@@ -69,6 +69,9 @@ public interface EventService {
 	
 	// 게시글 수정하기 서비스 
 	int updateEvent(Event e);
+
+	// 게시글 첨부파일 수정하기 서비스
+	int updateEventAttachment(List<Integer> fileNos, String status);
 	
 	// 게시글 삭제 서비스 
 	int deleteEvent(int eventNO);
@@ -109,10 +112,9 @@ public interface EventService {
 	// 쿠폰목록조회
 	ArrayList<Coupon> selectCouponList(Map<String, Object>params);
 
-	void deleteEventAttachment(List<Long> deleteFileIds);
-
 
 	int couponInsertEx(Map<String, Object> params);
+
 
 
 
